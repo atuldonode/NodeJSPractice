36e913e6e7d904f98f54721b958e676d8a8425ec
atuldonode <atuldonode72@gmail.com>
2021-11-06 19:27:10 +0530

    delete

--- zzz/src/db/conn.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/e-shop")
-.then( () => {
-console.log(`conection Succesfull`);
-}).catch((e) => {
-    console.log(e);
-}); 
