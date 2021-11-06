7fa9158991501478364a077eeaccef5099be8161
atuldonode <atuldonode72@gmail.com>
2021-11-06 19:25:03 +0530

    delete

--- API/models/mens.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const menSchema = new mongoose.Schema({
-    name:String,
-    mobile:Number,  
-});
-
-const Mens = mongoose.model(Mens, "menSchema")
-module.exports();
